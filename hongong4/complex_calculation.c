4e6015fc26eeb3ef8a2e7ea740b334b05d716e71
jo jae <jo jae@LAPTOP-O6O9H3F3>
2024-12-30 19:31:14 +0900

    complex_calulation_update

---
+++ hongong4/complex_calculation.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 10, b = 20;
+	int res = 2;
+
+	a += 20;		//a에 20을 더해주고 그 값을 a에 저장함
+	res *= b + 10;	//b에 10을 더한 값을 res 값에 곱해서 res에 저장
+
+	return 0;
+
+	
+}
