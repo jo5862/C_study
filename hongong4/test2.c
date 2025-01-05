78ca124f3cbb87eb6ebe60cc32e3cfe75ff36448
jo5862 <jo589396@naver.com>
2025-01-06 00:40:42 +0900

    test2_complete_update

--- hongong4/test2.c
+++ hongong4/test2.c
@@ -1,26 +1,30 @@
 #include <stdio.h>
 
-int main(void)
-{
-	int a;
+int main(void) {
+	int n;		//끝 정수 값을 입력하기 위한 변수 선언
 
 	printf("2 이상의 정수를 입력하세요 : ");
-	scanf("%d", &a);
-
-	int i, j,k; //변수 i,j 선언 
-		for (j = 2; j < 100; j++) //j가 0부터 4까지 증가하면서 5번 반복 
-		{
-			if (j=j*1)
-			{
-				printf("%d\n", j); //별 출력문
-			}
-			else
-			{
-				break;
-			}
-		}
-		printf("\n");
+	scanf("%d", &n);
 	
+	int i, j;		// 변수 i, j 선언
+	int cnt = 0;	// 5개 증가 후 줄 바꿈을 위한 변수 초기화
 
+	for ( i = 2; i <= n; i++)
+	{
+		for ( j = 2; j <= i; j++)	// j는 2부터 i - 1까지
+		{
+			if (i % j == 0)			//나머지 몫이 0이되면 
+				break;				//j에 대한 for문 탈출
+		}
+		if (i == j)					//예를들어 i값이 5일경우 j의 for문은 2,3,4,5까지 진행
+									//j의 마지막 값 5와 i의 값 5는 동일한걸 확인 가능
+		{
+			printf("%4d", i);		//i값 출력
+			cnt++;					//cnt 값 1증가
+			if (cnt % 5 == 0)		//cnt 값이 5가되면
+				printf("\n");		//줄 바꿈 출력
+		}
+	}
+	printf("\n");  // 마지막 줄 끝내기 위한 줄바꿈
 	return 0;
 }
